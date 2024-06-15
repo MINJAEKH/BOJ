9546099c065aee737d052eba688e5c988751cc32
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2024-06-15 18:32:41 +0900

    refactor : rename variables
    #

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,12 +1,13 @@
+# O(n)
 n, k = map(int,input().split())
-div = []
+divisor = []
 
 for i in range(1, n // 2+1) : 
     if n % i == 0 :
-        div.append(i)
-div.append(n)
+        divisor.append(i)
+divisor.append(n)
 
 if k > len(div) :
     print(0)
 else :
-    print(div[k-1])
+    print(divisor[k-1])
