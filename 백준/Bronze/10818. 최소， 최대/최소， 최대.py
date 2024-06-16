1836dce913d864828ecd1822ad7940604456c5e9
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2024-06-16 17:35:59 +0900

    [Bronze III] Title: 최소, 최대, Time: 388 ms, Memory: 153832 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,11 +1,3 @@
 N = int(input())
 N_lists = list(map(int, input().split()))
-
-mini = 1000000
-maxi = -1000000
-for num in N_lists :
-    if num >= maxi :
-        maxi = num
-    if num <= mini :
-        mini = num
-print(mini, maxi)
+print(min(N_lists), max(N_lists))
