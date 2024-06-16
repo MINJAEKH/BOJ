88354852ebeda78006ba8802617e92226fa6b1e7
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2024-06-16 17:39:14 +0900

    feat: add alternative solution

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,3 +1,16 @@
 N = int(input())
 N_lists = list(map(int, input().split()))
+
+# Sol 1
 print(min(N_lists), max(N_lists))
+
+# Sol 2
+mini = 1000000
+maxi = -1000000
+for num in N_lists :
+    if num >= maxi : # 20       35
+        maxi = num
+    if num <= mini : # 20   10      7
+        mini = num
+
+print(mini, maxi)
