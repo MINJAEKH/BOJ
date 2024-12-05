b6e7b5f44e271cdb8412f2f11978ceaed65abc11
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2024-12-05 20:18:33 +0900

    [Silver V] Title: 생일, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -2,14 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+info = []
 
-birth = []
-
-for _ in range(N) :
+for _ in range(N):
     name, day, month, year = input().strip().split()
-    birth.append([int(year),int(month),int(day),name])
+    if len(month) == 1 :
+        birthday = f'{year}-0{month}-{day}'
+    if len(day) == 1 :
+        birthday = f'{year}-{month}-0{day}'
+    info.append([birthday, name])
 
-birth.sort()
-#print(birth)
-print(birth[-1][-1])
-print(birth[0][-1])
+info.sort(key=lambda x: x[0])
+print(info[-1][-1])
+print(info[0][-1])
