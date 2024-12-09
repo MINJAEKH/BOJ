746a5de58700a8600f7a2e4a717ea3e6ca2578f3
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2024-12-09 17:17:54 +0900

    [Silver IV] Title: 빙고, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,35 +1,39 @@
-def check_bingo(board : list) :
+#import sys
+#input = sys.stdin.readline
+
+def check_bingo(board) :
     bingo = 0
-    # 행
+    # case 1 
     for row in range(5) :
         if board[row].count(0) == 5 :
             bingo += 1
-    # 열
+    # case 2
     for col in range(5) :
-        if all(board[row][col] == 0 for row in range(5)):
+        if [b[col] for b in board].count(0) == 5 :
             bingo += 1
-    # 대각선
-    if all(board[diagonal][diagonal] == 0 for diagonal in range(5)):
+    # case 3
+    left_diag = [board[row][row] for row in range(5)]
+    if left_diag.count(0) == 5 :
         bingo += 1
-    #대각선
-    if all(board[4-diagonal][diagonal] == 0 for diagonal in range(5)):
+    # case 4
+    right_diag = [board[row][4-row] for row in range(5)]
+    if right_diag.count(0) == 5 :
         bingo += 1
 
     return bingo
-
-board = [list(map(int, input().split())) for _ in range(5)]
-speaker = []
+    
+board = [list(map(int, input().strip().split())) for _ in range(5)]
+numbers = []
 for _ in range(5) :
-    speaker.extend(list(map(int, input().split())))
-
+    numbers.extend(list(map(int, input().strip().split())))
 
 for i in range(25) :
     for row in range(5) :
         for col in range(5) :
-            if speaker[i] == board[row][col] :
+            if numbers[i] == board[row][col] :
                 board[row][col] = 0
-    if i >= 11 :
-        result = check_bingo(board)
-        if result >= 3 :
-            print(i+1)
-            break
+
+    results = check_bingo(board)
+    if results >= 3 :
+        print(i+1)
+        break
