9c678b8357831e7ac35eeedb0712921bfaf30d58
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2024-12-12 01:40:51 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,5 +1,11 @@
-import math 
+dp = [[0]*30 for _ in range(30)]
+for m in range(1,30) :
+    dp[1][m] = m
 
+for n in range(2,30) :
+    for m in range(1, 30) :
+        dp[n][m] = dp[n-1][m-1] + dp[n][m-1]
+        
 for _ in range(int(input())) :
-    n, m = map(int, input().strip().split())
-    print(math.comb(m, n))
+    N, M = map(int, input().strip().split())
+    print(dp[N][M])
