afaccb72a0d966b54c6e6eaf74880e33bfea4dc7
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2026-02-12 16:53:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.42 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,12 @@
+from collections import defaultdict
+
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    participant_dict = defaultdict(int)
+
+    for name in participant :
+        participant_dict[name] += 1
+    for name in completion :
+        participant_dict[name] -= 1
     
-    for part, comp in zip(participant, completion) :
-        if part != comp :
-            return part
-    return participant[-1]
+    uncompleted = [k for k,v in participant_dict.items() if v == 1][0]
+    return uncompleted
