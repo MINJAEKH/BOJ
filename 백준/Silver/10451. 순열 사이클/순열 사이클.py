c471178b8fd761ea3b83893db0d32dd62c543eb2
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2026-02-23 22:21:12 +0900

    [Silver III] Title: 순열 사이클, Time: 344 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,18 +1,14 @@
-def dfs(permut, node, visited) :
-    visited.append(node)
-    #print(visited)
-    if permut[node] not in visited :
-        dfs(permut, permut[node], visited)
-
 for i in range(int(input())) :
     n = int(input())
     permut = [0] + list(map(int, input().rstrip().split()))
-    visited = []
+    visited = set()
     cnt = 0
 
-    for node in range(1, n+1) :
-        if node not in visited :
-            #print(node,':',permut[node])
-            dfs(permut, node, visited)
+    for v in range(1, n+1) :
+        if v not in visited :
             cnt += 1
+            while v not in visited :
+                visited.add(v) # 1 
+                v = permut[v] # 3
+                #print('visited:',visited) 
     print(cnt)
