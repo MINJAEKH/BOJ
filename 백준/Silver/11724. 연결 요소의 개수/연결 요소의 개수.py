44c43df917ea48eedc341b8fbbd9921f768f08e9
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2026-02-24 23:14:53 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 744 ms, Memory: 67020 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,34 +1,31 @@
+### 연결 요소의 개수
 import sys
+from collections import deque, defaultdict
+
 input = sys.stdin.readline
-import queue
 
-def bfs(graph, u, visited) :
-    visited.append(u)
-    my_queue = queue.Queue()
-    my_queue.put(u)
+n, m= map(int, input().rstrip().split())
+graph = {i : [] for i in range(1, n+1)}
 
-    while not my_queue.empty():
-        u = my_queue.get()
-        for v in graph[u] :
-            if v not in visited :
-                visited.append(v)
-                my_queue.put(v)
-        #print('visited:',visited, 'my_queue :',my_queue)
-
-n, m = map(int, input().strip().split())
-graph = {i: [] for i in range(1, n + 1)}
-cnt = 0
-visited = []
-
-# 인접리스트 
-for _ in range(m) : 
-    u, v = map(int, input().strip().split())
-    graph[u].append(v)
+for _ in range(m) :
+    v, u = map(int, input().rstrip().split())
     graph[v].append(u)
+    graph[u].append(v)
 
-for u in graph.keys() :
-    if u not in visited :
-        bfs(graph, u, visited)
-        cnt += 1
+visited = set()
+q = deque()
+result = 0
 
-print(cnt)
+for i in range(1, n+1) : # 1
+    if i not in visited : 
+        result += 1
+        visited.add(i) # 1
+        q.append(i)
+        while q :
+            v = q.popleft()  
+            for u in graph[v] :# 1,2
+                if u not in visited : 
+                    q.append(u) 
+                    visited.add(u)
+
+print(result)
