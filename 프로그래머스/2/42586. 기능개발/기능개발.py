197b974b5c3ed8fe17e7a74a82f22813d19898df
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2026-03-06 23:16:56 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,21 @@
-# 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수도 있고 
-# 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함계 배포됨
-# progresses :  배포되어야 하는 순서(작업의 진도가 100%로야 함)
-# speeds : 개발 속도
-# 각 배포마다 몇 개의 기능이 배포되는가?
 import math
 
 def solution(progresses, speeds):
-    deploys = 0
-    completed = []
+    completion_days = [
+        math.ceil((100 - progress) / speed)
+        for progress, speed in zip(progresses, speeds)
+    ]
+    
     answer = []
+    cnt = 0
+    current_release_days = completion_days[0]
+    for day in completion_days :
+        if current_release_days >= day :
+            cnt += 1
+        else :
+            answer.append(cnt)
+            current_release_days = day
+            cnt = 1
+    answer.append(cnt)
     
-    for progress, speed in zip(progresses, speeds) :
-        day = math.ceil((100 - progress) / speed)
-        completed.append(day)
-    
-    ind = 0
-    for i in range(len(completed)) :
-        if completed[ind] < completed[i] :
-            answer.append(i-ind)
-            ind = i
-    answer.append(i-ind+1)
     return answer
