79cbeef71b3eb4b9bc8839043d768bbd2877a324
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2026-03-24 13:23:28 +0900

    [Silver V] Title: 집합, Time: 3988 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,31 +1,24 @@
 import sys
 input = sys.stdin.readline
-
-S = set()
+s = set()
 
 for _ in range(int(input())) :
-    ls = input().split()
-    
-    if len(ls) == 1 :
-        if ls[0] == 'all' :
-            S = set(range(1, 21))
-        else : # empty 
-            S = set()
-            
-    else :
-        func, num = ls[0], int(ls[1])
-        
-        if func == 'add' :
-            S.add(num)
-        elif func == 'remove' :
-            S.discard(num) # 틀린 부분
-        elif func == 'check' :
-            if num in S :
-                print(1)
-            else :
-                print(0)
-        elif func == 'toggle' :
-            if num in S :
-                S.remove(num)
-            else :
-                S.add(num)
+    ops = input().split()
+
+    if len(ops) == 1:
+        op = ops[0]
+    else:
+        op, x_set = ops[0], {int(ops[1])}
+
+    if op == 'all':
+        s = set(range(1, 21))
+    elif op == 'empty' :
+        s.clear()
+    elif op == 'add' :
+        s = s | x_set
+    elif op == 'remove' :
+        s = s - x_set
+    elif op == 'check' :
+        print(1 if s & x_set else 0)
+    elif op == 'toggle' :
+        s = s ^ x_set
