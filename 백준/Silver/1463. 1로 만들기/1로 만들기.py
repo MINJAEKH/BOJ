84b066b011045392d249a0fe5a1b6ef07132ec6f
CHAMINJAE <109459615+MINJAEKH@users.noreply.github.com>
2026-04-08 10:03:16 +0900

    [Silver III] Title: 1로 만들기, Time: 452 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,19 @@
-n = int(input())
+import sys
 
-dp = [0]*(n+1)
+n = int(sys.stdin.readline())
 
-for i in range(2, n+1) :
-    dp[i] = dp[i-1] + 1
+dp = [0] * (n + 1)
+
+for i in range(2, n + 1):
+    # 현재 숫자에서 1을 빼는 경우
+    dp[i] = dp[i - 1] + 1
     
+    # 2로 나누어 떨어지는 경우
     if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+        
+    # 3으로 나누어 떨어지는 경우
     if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+        dp[i] = min(dp[i], dp[i // 3] + 1)
 
 print(dp[n])
