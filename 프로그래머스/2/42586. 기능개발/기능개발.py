23cb290c414e660605ec5b808554f8a3f841f224
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-06-22 12:50:13 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,26 @@
 import math
 
 def solution(progresses, speeds):
-    completion_days = [
-        math.ceil((100 - progress) / speed)
+    answer = []
+    
+    # 각 작업이 완료되기까지 남은 일수 계산
+    days_to_complete = [
+        math.ceil((100 - progress)/speed)
         for progress, speed in zip(progresses, speeds)
     ]
     
-    answer = []
     cnt = 0
-    current_release_days = completion_days[0]
-    for day in completion_days :
-        if current_release_days >= day :
+    max_days = days_to_complete[0]
+    for day in days_to_complete :
+        # 기준일보다 먼저 끝나거나 같이 끝나면 같은 날 배포
+        if max_days >= day :
             cnt += 1
         else :
             answer.append(cnt)
-            current_release_days = day
             cnt = 1
+            max_days = day
+    
+    # 마지막 배포 그룹 추가 
     answer.append(cnt)
     
     return answer
