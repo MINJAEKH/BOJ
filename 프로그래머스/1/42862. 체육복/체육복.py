964517bc3585b3395958caf322183981c7a88cee
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-07-21 11:42:36 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    # 여벌 체육복을 가져온 학생이 체육복을 도난당한 경우 고려
+    lost_set = sorted(set(lost) - set(reserve))
+    reserve_set = sorted(set(reserve) - set(lost))
+
+    for i in reserve_set :
+        if i-1 in lost_set :
+            lost_set.remove(i-1)
+        elif i+1 in lost_set :
+            lost_set.remove(i+1)
+    
+    return n - len(lost_set)
