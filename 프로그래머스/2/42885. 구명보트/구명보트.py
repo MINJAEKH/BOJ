944307fdbdf5453eadbdfcc034105161af9240e0
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-07-22 15:24:20 +0900

    [level 2] Title: 구명보트, Time: 7.61 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    people.sort()
+    
+    boats = 0
+    left, right = 0, len(people)-1
+    
+    while left <= right :
+        if people[left] + people[right] <= limit :
+            left += 1
+        right -= 1
+        boats += 1
+
+    return boats
