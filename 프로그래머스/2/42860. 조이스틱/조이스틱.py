eae63436afefe45ca9741607752023e5717f607f
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-07-24 22:50:30 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,26 +1,27 @@
+
 def solution(name):
     answer = 0
     n = len(name)
-    start, end = ord('A'), ord('Z')
     
     # 알파벳 상하 이동
-    for alphabet in name :
-        answer += min(ord(alphabet)-start, end-ord(alphabet)+1)
+    for alphabet in name:
+        answer += min(ord(alphabet) - ord('A'), ord('Z') - ord(alphabet) + 1)
     
     # 커서 좌우 이동
-    min_move = n - 1
-    i = 0
+    min_move = n - 1  # 기본값: 오른쪽으로 끝까지 이동
     
-    while i < n :
-        next_i = i + 1
-        while next_i < n and name[next_i] == 'A' :
-            next_i += 1
+    for i in range(n):
+        # i 위치 바로 다음부터 연속된 A의 개수 세기
+        cnt = 0
+        while i + 1 + cnt < n and name[i + 1 + cnt] == 'A':
+            cnt += 1
             
+        back_dist = i + 1 + cnt
+        
         min_move = min(
             min_move,
-            i * 2 + (n - next_i),
-            (n - next_i) * 2 + i)
-        
-        i = next_i 
+            i * 2 + (n-back_dist),
+            (n-back_dist) * 2 + i
+        )
         
     return answer + min_move
