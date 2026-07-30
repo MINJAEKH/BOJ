5f5ed1c1cd49511333a9d8b9fe74ef9e481f1a53
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-07-30 22:38:38 +0900

    [level 1] Title: 모의고사, Time: 1.18 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,21 +1,16 @@
 def solution(answers):
-    result = []
     scores = [0]*3
-    
     first = [1, 2, 3, 4, 5] # 5
     second = [2, 1, 2, 3, 2, 4, 2, 5] # 8
     third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # 10
     
-    for i in range(len(answers)) :
-        if answers[i] == first[i%5] :
+    for idx in range(len(answers)) :
+        if answers[idx] == first[idx%5] :
             scores[0] += 1
-        if answers[i] == second[i%8] :
+        if answers[idx] == second[idx%8] :
             scores[1] += 1
-        if answers[i] == third[i%10] :
+        if answers[idx] == third[idx%10] :
             scores[2] += 1
     
-    max_score = max(scores)
-    for idx, score in enumerate(scores) :
-        if score == max_score :
-            result.append(idx+1)
-    return result
+    m = max(scores)
+    return [i + 1 for i in range(len(scores)) if scores[i] == m]
