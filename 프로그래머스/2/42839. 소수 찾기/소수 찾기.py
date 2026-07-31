edcf0659099a6edd890b2ab808842a56ec3fd85a
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-07-31 17:09:42 +0900

    [level 2] Title: 소수 찾기, Time: 4.54 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,22 @@
 from itertools import permutations
 
-def is_decimal(x) :
+def is_prime(x) :
     if x < 2 :
         return False
-    
-    for i in range(2, int(pow(x, 0.5))+1) :
-        if x % i == 0 :
+    for divisor in range(2, int(x**0.5)+1) :
+        if x % divisor == 0 :
             return False
     return True
 
 def solution(numbers):
     answer = 0
-    checked = set()
+    decimal_set = set()
     num_list = list(numbers)
     
     for i in range(1, len(numbers)+1) :
-        for p in permutations(numbers, i): 
+        for p in permutations(num_list, i) :
             num = int(''.join(p))
-            if num not in checked and is_decimal(num):
-                answer += 1
-                #print(f'num = {num}, chekced = {checked}, answer = {answer}')
-            checked.add(num)
+            if num not in decimal_set and is_prime(num) :
+                answer +=1 
+                decimal_set.add(num)
     return answer
