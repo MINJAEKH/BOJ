4c04468e7272558d0272a1daee2b6a7ad7c14ad6
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-07-31 18:21:47 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,11 @@
-def solution(brown, yellow):
-    area = brown + yellow # 면적
+def solution(brown, yellow): 
+    area = brown + yellow
     
-    for div in range(3, int(pow(area,0.5))+1) :
-        if area % div == 0 :
-            w, h = max(area//div, div), min(area//div, div)
-            if (w-2)*(h-2) == yellow :
+    # 세로 길이는 최소 3 이상이어야 노란색이 들어갈 수 있음
+    for h in range(3, int(area**0.5)+1) :
+        # w >= h이므로 1~제곱근 사이 값은 h로 설정 가능
+        if area % h == 0 : 
+            w = area // h
+            
+            if (w-2) * (h-2) == yellow : 
                 return [w,h]
