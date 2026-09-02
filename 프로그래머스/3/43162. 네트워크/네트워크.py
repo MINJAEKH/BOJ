c302cfb9a66a867c55c1503a7890b0941a08b8d6
MinJae Cha <109459615+MINJAEKH@users.noreply.github.com>
2026-09-02 16:19:22 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,18 @@
 def dfs(n, i, visited, computers) :
     visited[i] = True
+    
     for j in range(n) :
-        if not visited[j] and computers[i][j] == 1 :
+        if computers[i][j] == 1 and not visited[j] :
             dfs(n, j, visited, computers)
     
+        
 def solution(n, computers):
     answer = 0
     visited = [False]*n
-    
+        
+        
     for i in range(n) :
-        if not visited[i] :
-            dfs(n, i, visited, computers)
+        if not visited[i] : #방문하지 않은 노드라면
+            dfs(n, i, visited, computers) 
             answer += 1
     return answer
